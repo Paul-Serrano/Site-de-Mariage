1d3fec01fb1c295cb5a38ff6e34f6a9af9ec6d48
paul Serrano <paul.serrano08374@gmail.com>
2022-03-02 10:01:26 +0100

    Cagnotte leetchi page

---
+++ view/leetchi.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once "../includes/_nav.php";
+
+?>
+
+<body>
+    <main class="leetchi-main">
+        <div class="leetchi-text-block">
+            <p>Texte d'explication à déterminer</p>
+        </div>
+        <div class="leetchi-link-block">
+            <a href="#" class="leetchi-link">
+                <button class="leetchi-btn">
+                    <p>Lien vers la cagnotte Leetchi</p>
+                </button>
+            </a>
+        </div>
+    </main>
+    <script src="../public/script.js"></script>
+</body>
