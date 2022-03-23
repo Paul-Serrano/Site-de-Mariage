69fff365d21a90b2ebc9e72b4ac45f9bb7331b0b
Serrano Paul <paul.serrano08374@gmail.com>
2022-03-23 18:10:45 +0100

    font change, contact header remove

--- controller/contact_post.php
+++ controller/contact_post.php
@@ -16,33 +16,38 @@ if(isset($_POST['contact-submit'])){
   
          if(true) {
 
-          if($_POST['mail-to'] = 'paul') {
+          if($_POST['mail-to'] == 'paul') {
             $mailTo = "paul.serrano08374@gmail.com";
           }
 
-          
+          $header="MIME-Version: 1.0\r\n";
+
+          if(isset($_SESSION['mail'])) {
+            $header.='From:'.$_SESSION['mail']."\n";
+          }
+          else {
+            $header.='From:'.$mailTo."\n";
+          }
+
+          $header.='Content-Type:text/html; charset="utf-8"'."\n";
+          $header.='Content-Transfer-Encoding: 8bit';
+
           $sujet = $_POST['mail-to-topic'];
-          $header ='From:'.$_SESSION['mail'].''; 
           $message='
           <html>
             <body>
-              <div align="center">
+              <div>
                 '.nl2br($_POST['message']).'
               </div>
             </body>
           </html>
           '; 
   
-        mail($mailTo, $sujet, $message, $header);
+        mail($mailTo, $sujet, $message);
        }
 
-         else {
-          return;
-        }
 
-
-        ?><pre><?php 
-    // var_dump($_SESSION['surname']);
+    ?><pre><?php 
     var_dump($mailTo);
     var_dump($message);
     var_dump($sujet);
